05f5a799cd7c6ab7f6b2b274ff8d96883c1dce05
Viktor Kortsanov <viktorkortsanov@gmail.com>
2025-03-27 10:15:02 +0200

    Updated server

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors({
   credentials: true,
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "http://localhost:5174"],
   optionsSuccessStatus: 200
 }));
 
