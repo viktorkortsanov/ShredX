3f26d57cd232bcbaea0a4c801f72f36a76f68243
Viktor Kortsanov <viktorkortsanov@gmail.com>
2025-03-30 13:05:47 +0300

    Updated contacts component and uptaded our team css

--- client/src/components/contacts/Contacts.jsx
+++ client/src/components/contacts/Contacts.jsx
@@ -1,36 +1,55 @@
 import useForm from '../../hooks/useForm';
 import './contact.css';
 import { useSelector } from 'react-redux';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
-    const userEmail = useSelector((state) => state.auth.user?.email);
-    console.log(userEmail);
-    
+    const user = useSelector((state) => state.auth?.user);
+
     const { values, handleChange, handleSubmit, error } = useForm({
         name: '',
         email: '',
         message: '',
     });
 
-    const handleFormSubmit = async (data) => {
-        console.log('Form data submitted:', data);
-        alert('Вашето съобщение беше изпратено!');
+    const handleFormSubmit = async () => {
+
+        const templateParams = {
+            username: user.username,
+            email: values.email,
+            subject: values.name,
+        };
+
+        try {
+            const response = await emailjs.send(
+                'service_b98xzqr',
+                'template_bvsfkrd',
+                templateParams,
+                'mxz5zqh2O_h0HA_5_'
+            );
+
+            if (response.ok) {
+                console.log(Success);
+            }
+        } catch (error) {
+            console.log(error);
+
+        }
     };
 
     return (
         <div className="contact-container">
             <div className="company-info">
                 <h2 className="title">Contacts</h2>
-                <p className="info"><strong>Name:</strong> ShredX</p>
-                <p className="info"><strong>Adress:</strong> Jungle Gym ulitsa "Kamchija", Svilengrad</p>
-                <p className="info"><strong>Phone number:</strong> +359 888 123 456</p>
-                <p className="info"><strong>Email:</strong> contact@shredx.com</p>
+                <p className="info"><img src="/images/adress.svg" alt="adress" className='icon-contact' />Jungle Gym ulitsa "Kamchija", Svilengrad</p>
+                <p className="info"><img src="/images/phone.svg" alt="adress" className='icon-contact' />+359 888 123 456</p>
+                <p className="info"><img src="/images/email.svg" alt="adress" className='icon-contact' />shredxcontact@gmail.com</p>
             </div>
 
             <div className="map-container">
                 <iframe
                     className="google-map"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2934.1640835873014!2d23.321867615768647!3d42.69770847916888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa857fc67a2a21%3A0x64b0b935f3d636bb!2z0KHQsNC0INGD0LvQuNGG0LjRjywg0KHQsNC0INCS0L7Qu9C40LXQvdC40YfQtdGB0LrQuNC5!5e0!3m2!1sbg!2sbg!4v1711467890123"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2943.184673041617!2d26.212789767483915!3d41.763043579694354!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14b46f8f8f8f8f8f%3A0x1234567890abcdef!2sYour+Location!5e0!3m2!1sbg!2sbg!4v1711467890123"
                     allowFullScreen=""
                     loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade"
@@ -58,7 +77,7 @@ const Contact = () => {
                             type="email"
                             id="email"
                             name="email"
-                            defaultValue={userEmail}
+                            defaultValue={user.email}
                             onChange={handleChange}
                             className="form-input-contact"
                         />
